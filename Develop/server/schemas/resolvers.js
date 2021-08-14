d49c0448e478f0bfb9d399753ad5fbbbdea55e8c
reetah589 <reetah589@gmail.com>
2021-08-14 15:05:52 -0500

    added login and adduser mutations tokens

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,6 +1,9 @@
+const { User, Book } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
+
   Query: {
     me: async (parent, args) => {
       const userData = await User.findOne({})
@@ -12,10 +15,39 @@ const resolvers = {
   },
 
   Mutation: {
-    login:
-    addUser:
-    saveBook:
-    removeBook:
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+    
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+    
+      const correctPw = await user.isCorrectPassword(password);
+    
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+    
+      const token = signToken(user);
+      return { token, user };
+    },
+
+
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user};
+    },
+
+    saveBook: async () => {
+
+    },
+
+    removeBook: async () => {
+
+    },
   },
 }  
   module.exports = resolvers;
