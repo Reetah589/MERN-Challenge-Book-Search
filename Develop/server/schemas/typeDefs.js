34c4387bf837cf6c27d64e19591c98a8c2038cab
reetah589 <reetah589@gmail.com>
2021-08-14 13:42:41 -0500

    added mutation to typedefs may still need to add more code

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,9 +1,18 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+
   type Query {
     me: User
   }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook: User
+    removeBook: User
+  }
+
 `;
 
 module.exports = typeDefs;
